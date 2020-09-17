2df787f4ee8ecf809f760d7c6db2545972eeab41
wecassidy <wec.notif.github@gmail.com>
2020-09-17 01:45:53 -0400

    Use getattr and setattr to dynamically get capture properties
    @property decorators are still used where special behaviour is needed

--- video.py
+++ video.py
@@ -28,7 +28,7 @@ class VideoCapture(cv2.VideoCapture):
         Set up the VideoCapture. All parameters are forwarded to the
         OpenCV VideoCapture object.
         """
-        super(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         self.stoppedIteration = False
 
     ## Context manager
@@ -57,18 +57,61 @@ class VideoCapture(cv2.VideoCapture):
             raise StopIteration
 
     ## Video properties
+    def __getattr__(self, name):
+        """
+        Used to convert
+        VideoCapture.get(cv2.CAP_PROP_EXAMPLE_PROPERTY) to
+        VideoCapture.example_property.
+        """
+        propertyName = f"CAP_PROP_{name.upper()}"
+        if propertyName in cv2.__dict__:
+            return self.get(cv2.__dict__[propertyName])
+        else:
+            return super().__getattr__(name)
+
+    def __setattr__(self, name, value):
+        propertyName = f"CAP_PROP_{name.upper()}"
+        if propertyName in cv2.__dict__:
+            return self.set(cv2.__dict__[propertyName], value)
+        else:
+            return super().__setattr__(name, value)
+
     @property
-    def fps(self):
-        return self.get(cv2.CAP_PROP_FPS)
+    def frame_width(self):
+        return int(self.get(cv2.CAP_PROP_FRAME_WIDTH))
+
+    @frame_width.setter
+    def set_frame_width(self, width):
+        return self.set(cv2.CAP_PROP_FRAME_WIDTH, width)
 
     @property
     def width(self):
-        return int(self.get(cv2.CAP_PROP_FRAME_WIDTH))
+        return self.frame_width
+
+    @width.setter
+    def set_width(self, width):
+        self.frame_width = width
+
+    @property
+    def frame_height(self):
+        return int(self.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    @frame_height.setter
+    def set_frame_height(self, height):
+        return self.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
     @property
     def height(self):
-        return int(self.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        return self.frame_height
+
+    @height.setter
+    def set_height(self, height):
+        self.frame_height = height
 
     @property
     def shape(self):
         return (self.width, self.height)
+
+    @shape.setter
+    def set_shape(self, shape):
+        self.width, self.height = shape
